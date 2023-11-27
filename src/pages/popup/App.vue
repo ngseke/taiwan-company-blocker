9114a24114f3edeb7ac745d959c016319c1a79c6
ngseke <ngseke@gmail.com>
2023-11-27 23:57:53 +0800

    feat: hide reveal buttons

--- src/pages/popup/App.vue
+++ src/pages/popup/App.vue
@@ -1,19 +1,9 @@
 <script setup lang="ts">
-import { REVEAL_MESSAGE_NAME, UNREVEAL_MESSAGE_NAME } from '../../modules/constants'
 import { formatPlatformName } from '../content/modules/platform'
 import PopupLayout from './components/PopupLayout.vue'
 import Statistic from './components/Statistic.vue'
-import { sendMessageToCurrentTab } from './modules/chrome'
 import { useContentMessage } from './composables/useContentMessage'
 import Button from '../../components/Button.vue'
-
-async function reveal () {
-  await sendMessageToCurrentTab(REVEAL_MESSAGE_NAME)
-}
-
-async function unreveal () {
-  await sendMessageToCurrentTab(UNREVEAL_MESSAGE_NAME)
-}
 
 function openOptions () {
   chrome.runtime.openOptionsPage()
@@ -35,15 +25,7 @@ const { platformName, blockedCount } = useContentMessage()
       </div>
 
       <div class="flex flex-wrap gap-2">
-        <div class="w-2/5">
-          <Button @click="openOptions">管理篩選關鍵詞</Button>
-        </div>
-        <div class="flex-none">
-          <Button @click="reveal">Reveal</Button>
-        </div>
-        <div class="flex-none">
-          <Button @click="unreveal">Unreveal</Button>
-        </div>
+        <Button @click="openOptions">管理篩選關鍵詞</Button>
       </div>
     </div>
   </PopupLayout>
