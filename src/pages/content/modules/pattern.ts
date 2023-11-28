eaa75b116f398349d67d2b663e80163eae21ba89
ngseke <ngseke@gmail.com>
2023-11-28 21:35:48 +0800

    feat: support matching regular expression

--- src/pages/content/modules/pattern.ts
+++ src/pages/content/modules/pattern.ts
@@ -1,17 +1,45 @@
 import { isMatch } from 'matcher'
 import { type Nullish } from '../../../types/Nullish'
 import { type PlatformName } from './platform'
+import { groupBy } from 'lodash-es'
+import RegexParser from 'regex-parser'
+
+function isRegexpLiteral (maybeRegexpLiteral: string) {
+  const isFormatValid = /^\/.*\/$/.test(maybeRegexpLiteral)
+  try {
+    const regex = new RegExp(maybeRegexpLiteral)
+    return isFormatValid && Boolean(regex)
+  } catch (e) {
+    return false
+  }
+}
 
 export function match (input: string, pattern: string | string[]) {
   const patterns = Array.isArray(pattern)
     ? pattern.map(string => string.trim())
-    : pattern.trim()
+    : [pattern.trim()]
 
-  return isMatch(
-    input.trim(),
-    patterns,
+  const { regexpPatterns, stringPatterns } = groupBy(patterns, (pattern) => (
+    isRegexpLiteral(pattern) ? 'regexpPatterns' : 'stringPatterns'
+  ))
+
+  const trimmedInput = input.trim()
+
+  const isMatchedStringPatterns = isMatch(
+    trimmedInput,
+    stringPatterns ?? [],
     { caseSensitive: false }
   )
+  const isMatchedRegexpPatterns = (regexpPatterns ?? [])
+    .some((regexpLiteral) => {
+      try {
+        return RegexParser(regexpLiteral).test(trimmedInput)
+      } catch (e) {
+        return false
+      }
+    })
+
+  return isMatchedStringPatterns || isMatchedRegexpPatterns
 }
 
 export type PatternType = 'jobTitle' | 'companyName'
