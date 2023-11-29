493b1799c63d7df39a7c35e390a74e94718c5494
ngseke <ngseke@gmail.com>
2023-11-29 14:25:48 +0800

    feat: update footer

--- src/pages/options/components/Footer.vue
+++ src/pages/options/components/Footer.vue
@@ -1,25 +1,29 @@
 <script setup lang="ts">
 const links = [
   {
+    title: '問題回報',
+    url: 'https://github.com/ngseke/taiwan-company-blocker/issues',
+  },
+  {
     title: 'GitHub',
     url: 'https://github.com/ngseke/taiwan-company-blocker',
   },
   {
-    title: 'Issues',
-    url: 'https://github.com/ngseke/taiwan-company-blocker/issues',
+    title: 'Chrome Web Store',
+    url: 'https://chromewebstore.google.com/detail/hmkkfmjfinbllbbkgabkeponkhckmijk',
   },
 ]
 </script>
 
 <template>
   <footer>
-    <ul class="flex divide-x divide-neutral-700">
+    <ul class="-mx-2 flex divide-x divide-neutral-700">
       <li
         v-for="{ title, url } in links"
         :key="title"
         class="px-2"
       >
-        <a :href="url" target="_blank">{{ title }}</a>
+        <a class="hover:underline" :href="url" target="_blank">{{ title }}</a>
       </li>
     </ul>
   </footer>
