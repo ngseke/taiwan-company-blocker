f4bc224ea68086f628beb2d91b2bb5e2deffb262
ngseke <ngseke@gmail.com>
2023-11-29 22:28:46 +0800

    feat: update InstructionArticle

--- src/pages/options/components/InstructionArticle.vue
+++ src/pages/options/components/InstructionArticle.vue
@@ -17,6 +17,7 @@ import Code from './Code.vue'
     </p>
     <ol class="list-inside list-decimal">
       <li><Code>/(內|外)場/</Code> 會匹配到包含「<i>內場</i>」或「<i>外場</i>」的項目</li>
+      <li><Code>/BACK(-?)END/i</Code> 會匹配到包含無論大小寫的「<i>Backend</i>」或「<i>Back-end</i>」的項目</li>
       <li><Code>/^[\u4e00-\u9fff]+$/</Code> 會匹配到<strong>完全</strong>只有出現中文的項目</li>
     </ol>
   </article>
