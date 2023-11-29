fdd1e48cb60dfede58cd459a42bf68e1aea46ec5
ngseke <ngseke@gmail.com>
2023-11-29 08:18:53 +0800

    refactor: optimize useChromeStorage to prevent unnecessary changes

--- src/composables/useChromeStorage.ts
+++ src/composables/useChromeStorage.ts
@@ -1,18 +1,23 @@
 import { type Ref, onMounted, ref, watch } from 'vue'
 import { type SyncStorageKey, type SyncStorageSchema, getSyncStorage, setSyncStorage } from '../modules/storage'
 import { useChromeStorageListener } from './useChromeStorageListener'
+import { isEqual } from 'lodash-es'
 
 export function useChromeStorage <
   Key extends SyncStorageKey
 > (key: Key) {
   const value: Ref<SyncStorageSchema[Key] | null> = ref(null)
 
-  async function handler () {
+  onMounted(async () => {
     value.value = await getSyncStorage(key)
-  }
+  })
+  useChromeStorageListener((changes) => {
+    if (!(key in changes)) return
+    const { newValue, oldValue } = changes[key]
+    if (isEqual(newValue, oldValue)) return
 
-  onMounted(handler)
-  useChromeStorageListener(handler)
+    value.value = newValue
+  })
 
   watch(value, (value) => {
     if (value == null) return
