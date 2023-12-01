b537ee6c1a0bf3de2b5c90135cfbe0e955f7adfb
ngseke <ngseke@gmail.com>
2023-12-01 12:50:38 +0800

    test: fix test

--- src/pages/options/App.test.ts
+++ src/pages/options/App.test.ts
@@ -3,11 +3,12 @@ import { flushPromises, mount } from '@vue/test-utils'
 
 import { mockChrome } from '../../../__tests__/chrome'
 import App from './App.vue'
+import { createTestingPinia } from '@pinia/testing'
 
 test('options app should be rendered without any errors and warnings', async () => {
   mockChrome()
 
-  mount(App)
+  mount(App, { global: { plugins: [createTestingPinia()] } })
   await flushPromises()
 
   expect(console.error).toBeCalledTimes(0)
