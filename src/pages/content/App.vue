8a67192bb56fc1aedcdd6f16268906804f92e3a7
ngseke <ngseke@gmail.com>
2023-12-01 16:49:27 +0800

    feat: create search shortcut

--- src/pages/content/App.vue
+++ src/pages/content/App.vue
@@ -9,6 +9,8 @@ import { OPEN_OPTIONS_PAGE_MESSAGE_NAME } from '../../modules/constants'
 import Checkbox from '../../components/Checkbox.vue'
 import { appendPattern } from '../../modules/storage'
 import { useScrollLock } from './composables/useScrollLock'
+import { type Nullish } from '../../types/Nullish'
+import SearchLink from './components/SearchLink.vue'
 
 const dialogRef = ref<HTMLDialogElement | null>(null)
 const isOpened = ref(false)
@@ -27,15 +29,23 @@ watch(isOpened, (isOpened) => { isLocked.value = isOpened })
 
 const isJobTitleChecked = ref(false)
 const isCompanyNameChecked = ref(false)
+
+const jobTitle = ref<Nullish<string>>(null)
+const companyName = ref<Nullish<string>>(null)
+
 const jobTitleDraft = ref('')
 const companyNameDraft = ref('')
 
-useEmitter(CLICK_ITEM_ACTION, ({ jobTitle, companyName }) => {
+useEmitter(CLICK_ITEM_ACTION, (payload) => {
   open()
   isJobTitleChecked.value = false
   isCompanyNameChecked.value = false
-  jobTitleDraft.value = jobTitle ?? ''
-  companyNameDraft.value = companyName ?? ''
+
+  jobTitle.value = payload.jobTitle
+  companyName.value = payload.companyName
+
+  jobTitleDraft.value = payload.jobTitle ?? ''
+  companyNameDraft.value = payload.companyName ?? ''
 })
 
 function openOptions () {
@@ -56,6 +66,8 @@ const isSubmitDisabled = computed(() => !(
   (isJobTitleChecked.value && jobTitleDraft.value.trim()) ||
   (isCompanyNameChecked.value && companyNameDraft.value.trim())
 ))
+
+const searchSuffixes = ['PTT', '面試', '薪水']
 </script>
 
 <template>
@@ -65,7 +77,7 @@ const isSubmitDisabled = computed(() => !(
       class="overflow-visible bg-transparent p-0 backdrop:bg-neutral-900/70 backdrop:backdrop-blur-[1px]"
       @close="isOpened = false"
     >
-      <div v-if="isOpened" class="flex w-[420px] max-w-full flex-col gap-4 rounded-lg bg-neutral-900 p-4 text-neutral-300 shadow-2xl">
+      <div v-if="isOpened" class="flex w-[420px] max-w-full flex-col gap-4 rounded-xl bg-neutral-900 p-5 text-neutral-300 shadow-2xl">
         <Header />
         <div class="flex flex-col gap-4">
           <div class="flex items-center gap-2">
@@ -97,6 +109,26 @@ const isSubmitDisabled = computed(() => !(
           >
             封鎖
           </Button>
+        </div>
+
+        <hr class="border-neutral-800">
+
+        <div class="flex flex-wrap gap-3">
+          <span>搜尋公司：</span>
+          <SearchLink
+            :disabled="!companyName"
+            :href="`https://www.google.com/search?q=${companyName}`"
+          >
+            Google
+          </SearchLink>
+          <SearchLink
+            v-for="suffix in searchSuffixes"
+            :key="suffix"
+            :disabled="!companyName"
+            :href="`https://www.google.com/search?q=${companyName} ${suffix}`"
+          >
+            {{ suffix }}
+          </SearchLink>
         </div>
       </div>
     </dialog>
---
+++ src/pages/content/components/SearchLink.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import { type Nullish } from '../../../types/Nullish'
+import SearchIcon from './SearchIcon.vue'
+
+const props = defineProps<{
+  href?: Nullish<string>
+  disabled?: boolean
+}>()
+
+function handleClick (event: Event) {
+  if (props.disabled) {
+    event.preventDefault()
+  }
+}
+</script>
+
+<template>
+  <a
+    class="inline-flex items-center gap-1"
+    :class="{
+      'cursor-default opacity-50': disabled,
+      'hover:underline': !disabled,
+    }"
+    :href=" href ?? undefined"
+    target="_blank"
+    @click="handleClick"
+  >
+    <SearchIcon :size="14" />
+    <slot />
+  </a>
+</template>
