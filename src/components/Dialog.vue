afcfc4bdaa5cd1d12ec23a2af410fec6885f83c7
ngseke <ngseke@gmail.com>
2023-12-11 12:55:39 +0800

    feat: add ability to close dialog on click outside

--- src/components/Dialog.vue
+++ src/components/Dialog.vue
@@ -5,12 +5,14 @@ import { onMounted, ref, watch } from 'vue'
 const props = withDefaults(defineProps<{
   width?: string
   open?: boolean
+  closeOnClickOutside?: boolean
 }>(), {
   width: '420px',
   open: false,
+  closeOnClickOutside: false,
 })
 
-defineEmits<{
+const emit = defineEmits<{
   'close': []
 }>()
 
@@ -28,16 +30,27 @@ onMounted(() => {
     }
   }, { immediate: true })
 })
+
+function close () {
+  emit('close')
+}
+
+function handleClickDialog (event: Event) {
+  if (!props.closeOnClickOutside) return
+  if (event.target !== dialogRef.value) return
+  close()
+}
 </script>
 
 <template>
   <dialog
     ref="dialogRef"
-    class="overflow-visible bg-transparent p-0 backdrop:bg-neutral-900/80 backdrop:backdrop-blur-[1px]"
-    @close="$emit('close')"
+    class="max-h-full overflow-auto bg-transparent p-4 backdrop:bg-neutral-900/80 backdrop:backdrop-blur-[1px]"
+    @click="handleClickDialog"
+    @close="close"
   >
     <div
-      class="max-w-full rounded-xl bg-neutral-900 p-5 text-neutral-300 shadow-2xl"
+      class="max-h-full max-w-full rounded-xl bg-neutral-900 p-5 text-neutral-300 shadow-2xl"
       :style="{ width }"
     >
       <slot />
