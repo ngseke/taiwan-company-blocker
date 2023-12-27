bf6e7b0892b04a52aa12b30ec21547cb89a7fa17
ngseke <ngseke@gmail.com>
2023-12-28 01:25:27 +0800

    refactor: remove `v-if` on Codemirror to fix scroll to center bug

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts">
-import { computed, ref } from 'vue'
+import { computed } from 'vue'
 import { Codemirror } from 'vue-codemirror'
-import { useVModel } from '@vueuse/core'
 import { EditorView } from '@codemirror/view'
 import { rulesLanguage } from '../modules/codeMirror'
 import { codeMirrorTheme } from '../modules/codeMirrorTheme'
@@ -16,7 +15,7 @@ const props = withDefaults(defineProps<{
   height: 350,
 })
 
-const emit = defineEmits<{
+defineEmits<{
   'update:modelValue': [value: string]
 }>()
 
@@ -25,25 +24,15 @@ const extensions = computed(() => [
   codeMirrorTheme,
   ...(props.lineWrapping ? [EditorView.lineWrapping] : []),
 ])
-
-const vModel = useVModel(props, 'modelValue', emit)
-
-const codeMirrorRef = ref<InstanceType<typeof Codemirror> | null>(null)
-async function handleReady () {
-  await new Promise((resolve) => setTimeout(resolve, 0))
-  codeMirrorRef.value?.$el.querySelector('.cm-scroller')?.scrollTo?.(0, 0)
-}
 </script>
 
 <template>
   <Codemirror
-    v-if="vModel != null"
-    ref="codeMirrorRef"
-    v-model="vModel"
     :disabled="disabled"
     :extensions="extensions"
     :indentWithTab="false"
+    :modelValue="modelValue ?? undefined"
     :style="{ height: typeof height === 'string' ? height : `${height}px` }"
-    @ready="handleReady"
+    @update:modelValue="$emit('update:modelValue', $event)"
   />
 </template>
