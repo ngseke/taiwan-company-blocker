181049dc1de1def174dffd2781e02a65403bf7c4
ngseke <ngseke@gmail.com>
2023-12-27 15:10:24 +0800

    feat: create rule storage actions

--- src/modules/ruleStorageAction.ts
+++ src/modules/ruleStorageAction.ts
@@ -1,5 +1,24 @@
-import { type RuleType } from './rule'
+import { type RuleType, convertRulesStringToArray } from './rule'
 import { loadRules, saveRules } from './storage'
+
+export async function removeRule (type: RuleType, rule: string) {
+  const rawRules = await loadRules(type)
+  const list = convertRulesStringToArray(rawRules)
+  const filteredList = list.filter((item) => item !== rule)
+
+  await saveRules(type, filteredList.join('\n'))
+}
+
+export async function replaceRule (type: RuleType, oldRule: string, newRule: string) {
+  const rawRules = await loadRules(type)
+  const list = convertRulesStringToArray(rawRules)
+  const replacedList = list.map((item) => {
+    if (item === oldRule) return newRule
+    return item
+  })
+
+  await saveRules(type, replacedList.join('\n'))
+}
 
 export async function appendRule (type: RuleType, rule: string) {
   const rules = await loadRules(type)
