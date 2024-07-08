323e783f6db83cda8a60d36d52b1a0215a78cd84
ngseke <ngseke@gmail.com>
2024-07-08 20:04:07 +0800

    feat: add subscription list recommendation alert

--- src/pages/options/components/AddSubscriptionDialog.vue
+++ src/pages/options/components/AddSubscriptionDialog.vue
@@ -4,6 +4,8 @@ import Dialog from '../../../components/Dialog.vue'
 import Input from '../../../components/Input.vue'
 import Button from '../../../components/Button.vue'
 import { validateUrl } from '../../../modules/validateUrl'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faStar } from '@fortawesome/free-solid-svg-icons'
 
 interface Response {
   name: string
@@ -50,10 +52,21 @@ function cancel () {
   <Dialog :open="Boolean(state)" @close="cancel">
     <div class="flex flex-col gap-4">
       <div class="text-base font-medium">新增訂閱</div>
+
       <div class="flex flex-col gap-4">
         <Input v-model="nameDraft" label="名稱" :maxLength="100" />
         <Input v-model="urlDraft" label="URL" />
       </div>
+
+      <a
+        class="block rounded-lg bg-neutral-800 p-3 py-2 text-neutral-200 duration-200"
+        href="https://github.com/ngseke/company-list"
+        target="_blank"
+      >
+        <FontAwesomeIcon :icon="faStar" />
+        也可以查看由此擴充功能作者維護的<span class="underline">公司訂閱列表</span>，快速過濾如博弈、外包等特定產業。
+      </a>
+
       <div class="flex justify-end gap-2">
         <Button @click="cancel">取消</Button>
         <Button
