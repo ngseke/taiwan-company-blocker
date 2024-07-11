9ebae4dc5ec0c903a39003115510d59043b74487
ngseke <ngseke@gmail.com>
2024-07-11 20:20:43 +0800

    feat: create version alert

--- src/pages/options/App.vue
+++ src/pages/options/App.vue
@@ -6,12 +6,14 @@ import Header from './components/Header.vue'
 import EnableOptions from './components/EnableOptions.vue'
 import RulesOptions from './components/RulesOptions.vue'
 import SubscriptionOptions from './components/SubscriptionOptions.vue'
+import VersionUpdatedAlert from './components/VersionUpdatedAlert.vue'
 </script>
 
 <template>
   <div class="container max-w-2xl p-4">
-    <div class="flex flex-col gap-4">
+    <div class="grid gap-4">
       <Header />
+      <VersionUpdatedAlert />
 
       <Card>
         <EnableOptions />
---
+++ src/pages/options/components/VersionUpdatedAlert.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faCheck, faXmark } from '@fortawesome/free-solid-svg-icons'
+import { useVersion } from '../../../composables/useVersion'
+
+const version = `v${APP_VERSION}`
+
+const { isUpdated, updatePreviousVersion } = useVersion()
+
+function handleDismiss () {
+  updatePreviousVersion()
+}
+</script>
+
+<template>
+  <div
+    v-if="isUpdated"
+    class="rounded-lg bg-neutral-800 p-3 py-2 duration-200"
+  >
+    <div class="flex gap-2">
+      <div class="flex-1">
+        <FontAwesomeIcon class="mr-1" :icon="faCheck" />
+        已成功更新至 <span class="font-mono">{{ version }}</span>。
+
+        <a
+          class="underline hover:text-red-500"
+          href="https://github.com/ngseke/taiwan-company-blocker/releases"
+          target="_blank"
+        >
+          查看版本記錄
+        </a>
+      </div>
+
+      <button
+        class="inline-flex items-center justify-center p-1 hover:text-red-500"
+        type="button"
+        @click="handleDismiss"
+      >
+        <FontAwesomeIcon :icon="faXmark" />
+      </button>
+    </div>
+  </div>
+</template>
