c61be292cbed7bf5ee9547410ecb47864bc2fac4
ngseke <ngseke@gmail.com>
2024-07-11 20:21:07 +0800

    feat: create print storage button for debug

--- src/pages/options/components/Header.vue
+++ src/pages/options/components/Header.vue
@@ -3,10 +3,18 @@ import { computed, ref } from 'vue'
 import icon from '../../../assets/img/icon.png'
 import { useChromeStorage } from '../../../composables/useChromeStorage'
 import { DEBUGGER_ENABLED_STORAGE_KEY } from '../../../modules/storage'
+import Button from '../../../components/Button.vue'
 
 const isDebuggerEnabled = useChromeStorage(DEBUGGER_ENABLED_STORAGE_KEY)
 const count = ref(0)
 const shouldShowDebuggerButton = computed(() => count.value >= 3)
+
+async function handleClickPrintStorage () {
+  // eslint-disable-next-line no-console
+  console.info('local storage', await chrome.storage.local.get(null))
+  // eslint-disable-next-line no-console
+  console.info('sync storage', await chrome.storage.sync.get(null))
+}
 </script>
 
 <template>
@@ -20,11 +28,17 @@ const shouldShowDebuggerButton = computed(() => count.value >= 3)
       Taiwan Company Blocker
     </h1>
 
-    <div v-if="shouldShowDebuggerButton" class="mt-4">
+    <div v-if="shouldShowDebuggerButton" class="mt-4 flex flex-col items-start gap-2">
       <label>
         <input v-model="isDebuggerEnabled" type="checkbox">
         Enable Debugger
       </label>
+      <Button
+        type="button border"
+        @click="handleClickPrintStorage"
+      >
+        Print Storage
+      </Button>
     </div>
   </div>
 </template>
