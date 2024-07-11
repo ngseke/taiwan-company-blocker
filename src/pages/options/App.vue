810dc6133e60b78056f1d69531a8188549fc13b2
ngseke <ngseke@gmail.com>
2024-07-11 22:08:52 +0800

    feat: create about page

--- src/pages/options/App.vue
+++ src/pages/options/App.vue
@@ -1,42 +1,55 @@
 <script setup lang="ts">
 import Card from './components/Card.vue'
-import Footer from './components/Footer.vue'
 import BlockMethodOptions from './components/BlockMethodOptions.vue'
 import Header from './components/Header.vue'
 import EnableOptions from './components/EnableOptions.vue'
 import RulesOptions from './components/RulesOptions.vue'
 import SubscriptionOptions from './components/SubscriptionOptions.vue'
 import VersionUpdatedAlert from './components/VersionUpdatedAlert.vue'
+import Sidebar from './components/Sidebar.vue'
+import { ref } from 'vue'
+import About from './components/About.vue'
+
+const list = [
+  { label: '設定', value: 'setting' },
+  { label: '關於', value: 'about' },
+]
+const current = ref(list[0].value)
 </script>
 
 <template>
   <div class="container flex max-w-5xl flex-wrap gap-x-8 gap-y-4 px-4">
-    <div class="sticky top-0 h-full flex-1 py-8">
+    <div class="top-0 grid h-full flex-1 gap-4 pt-2 lg:sticky lg:py-8">
       <Header />
+      <Sidebar v-model="current" :list="list" />
     </div>
 
-    <div class="grid w-full gap-4 lg:w-9/12 lg:py-8">
-      <VersionUpdatedAlert />
+    <div class="w-full pb-8 lg:w-9/12 lg:py-12">
+      <div v-show="current === 'setting'" class="grid w-full gap-4">
+        <VersionUpdatedAlert />
 
-      <Card>
-        <EnableOptions />
-      </Card>
+        <Card>
+          <EnableOptions />
+        </Card>
 
-      <Card>
-        <RulesOptions />
-      </Card>
+        <Card>
+          <RulesOptions />
+        </Card>
 
-      <Card>
-        <SubscriptionOptions />
-      </Card>
+        <Card>
+          <SubscriptionOptions />
+        </Card>
 
-      <Card>
-        <BlockMethodOptions />
-      </Card>
+        <Card>
+          <BlockMethodOptions />
+        </Card>
+      </div>
 
-      <Card>
-        <Footer />
-      </Card>
+      <div v-show="current === 'about'" class="grid w-full gap-4">
+        <Card>
+          <About />
+        </Card>
+      </div>
     </div>
   </div>
 </template>
---
+++ src/pages/options/components/About.vue
@@ -0,0 +1,76 @@
+<script setup lang="ts">
+const version = `v${APP_VERSION}`
+
+const links = [
+  {
+    title: '版本記錄',
+    url: 'https://github.com/ngseke/taiwan-company-blocker/releases',
+  },
+  {
+    title: '問題回報',
+    url: 'https://github.com/ngseke/taiwan-company-blocker/issues',
+  },
+  {
+    title: 'GitHub',
+    url: 'https://github.com/ngseke/taiwan-company-blocker',
+  },
+  {
+    title: 'Chrome Web Store',
+    url: 'https://chromewebstore.google.com/detail/hmkkfmjfinbllbbkgabkeponkhckmijk',
+  },
+]
+</script>
+
+<template>
+  <section class="grid gap-4">
+    <div class="grid gap-0.5">
+      <h1 class="text-2xl font-bold">
+        台灣求職網封鎖神器
+      </h1>
+
+      <span class="mt-1 font-mono text-neutral-600">
+        {{ version }}
+      </span>
+    </div>
+
+    <hr class="border-neutral-800">
+
+    <h2 class="text-lg font-medium">關於此擴充功能</h2>
+    <p>
+      這是啟發自 <i>uBlacklist</i> 的免費開源專案。若你在使用時遇到任何問題，請不吝來信至
+      <a class="underline hover:text-red-500" href="mailto:ngseke@gmail.com">ngseke@gmail.com</a>
+      與我聯繫，或是到
+      <a class="underline hover:text-red-500" href="https://github.com/ngseke/taiwan-company-blocker/issues" target="_blank">GitHub</a>
+      發送 Issue 回報，也歡迎有志之士提交
+      <a class="underline hover:text-red-500" href="https://github.com/ngseke/taiwan-company-blocker/pulls" target="_blank">Pull Request</a>
+      來貢獻程式碼。
+    </p>
+
+    <h2 class="mt-4 text-lg font-medium">隱私權聲明</h2>
+    <p>
+      《台灣求職網封鎖神器》不會收集任何你的個人資訊。
+    </p>
+
+    <h2 class="mt-4 text-lg font-medium">相關連結</h2>
+    <ul class="-mx-2 flex divide-x divide-neutral-700">
+      <li
+        v-for="{ title, url } in links"
+        :key="title"
+        class="px-2"
+      >
+        <a class="hover:underline" :href="url" target="_blank">{{ title }}</a>
+      </li>
+    </ul>
+
+    <p class="mt-4 text-xs">
+      Made with 🍺 by
+      <a
+        class="font-mono underline hover:text-red-500"
+        href="https://ngseke.me"
+        target="_blank"
+      >
+        @ngseke
+      </a>
+    </p>
+  </section>
+</template>
