ad4c2aba60e71b2f6fd5cf93a2ee8bc4874b3f4c
ngseke <ngseke@gmail.com>
2024-07-13 12:59:00 +0800

    fix: fix type

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 import packageJson from './package.json'
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue() as any],
   test: {
     setupFiles: [
       './__tests__/setup.ts',
