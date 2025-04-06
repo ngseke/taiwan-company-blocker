f997d3733917b357e41253764173aafb19c50c9b
ngseke <ngseke@gmail.com>
2025-04-06 19:06:43 +0800

    fix: cast input to string for regex pattern matching

--- src/modules/pattern.ts
+++ src/modules/pattern.ts
@@ -148,7 +148,7 @@ export function match (input: Nullish<string>, ruleOrRules: string | string[]) {
     stringPatterns ?? [],
   )
   const isMatchedRegexpPatterns = regexPatterns
-    .some((regex) => regex.test(input))
+    .some((regex) => regex.test(input as string))
 
   return isMatchedStringPatterns || isMatchedRegexpPatterns
 }
