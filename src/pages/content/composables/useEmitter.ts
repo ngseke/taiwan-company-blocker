b6edcbe5295e9f3fc31ac504c8723d87b71a24f6
ngseke <ngseke@gmail.com>
2025-04-22 21:30:50 +0800

    fix: fix type

--- src/pages/content/composables/useEmitter.ts
+++ src/pages/content/composables/useEmitter.ts
@@ -1,15 +1,18 @@
 import { onMounted, onUnmounted } from 'vue'
 import { type EventTypes, emitter } from '../modules/emitter'
+import type EventEmitter from 'eventemitter3'
 
 export function useEmitter <EventName extends keyof EventTypes> (
   eventName: EventName,
   handler: EventTypes[EventName]
 ) {
+  type Handler = EventEmitter.EventListener<EventTypes, EventName>
+
   onMounted(() => {
-    emitter.on(eventName, handler)
+    emitter.on(eventName, handler as Handler)
 
     onUnmounted(() => {
-      emitter.removeListener(eventName, handler)
+      emitter.removeListener(eventName, handler as Handler)
     })
   })
 }
