ec8994398ba69284a7f35b3c7ff42ec40bb84f89
ngseke <ngseke@gmail.com>
2025-04-22 23:09:32 +0800

    feat: update activator icon

--- src/pages/content/modules/activator.ts
+++ src/pages/content/modules/activator.ts
@@ -11,7 +11,9 @@ export function renderActivator () {
         class="${style.activator}"
         title="封鎖"
       >
-        <svg shape-rendering="geometricPrecision" width="20" height="20" viewBox="0 0 512 512" style="fill: currentColor;"><path d="M367.2 412.5L99.5 144.8C77.1 176.1 64 214.5 64 256c0 106 86 192 192 192c41.5 0 79.9-13.1 111.2-35.5zm45.3-45.3C434.9 335.9 448 297.5 448 256c0-106-86-192-192-192c-41.5 0-79.9 13.1-111.2 35.5L412.5 367.2zM0 256a256 256 0 1 1 512 0A256 256 0 1 1 0 256z"/></svg>
+        <svg shape-rendering="geometricPrecision" width="20" height="20" viewBox="0 0 512 512" style="fill: currentColor;">
+          <path d="M144.8 412.5L412.5 144.8C434.9 176.1 448 214.5 448 256c0 106-86 192-192 192c-41.5 0-79.9-13.1-111.2-35.5zm-45.3-45.3C77.1 335.9 64 297.5 64 256c0-106 86-192 192-192c41.5 0 79.9 13.1 111.2 35.5L99.5 367.2zM512 256a256 256 0 1 0 -512 0a256 256 0 1 0 512 0z"/>
+        </svg>
       </button>
     </div>
   `)
