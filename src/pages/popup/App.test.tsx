b47f44664e0b64e2ef2576fa2ee6f9f0a896b02a
ngseke <ngseke@gmail.com>
2025-04-24 21:49:41 +0800

    test: fix test

--- src/pages/popup/App.test.tsx
+++ src/pages/popup/App.test.tsx
@@ -1,15 +1,16 @@
 /* eslint-disable no-console */
-import { flushPromises, mount } from '@vue/test-utils'
+import { act, render } from '@testing-library/react'
 
 import { mockChrome } from '../../../__tests__/chrome'
-import App from './App.vue'
+import { App } from './App.tsx'
 
 test('popup app should be rendered without any errors and warnings', async () => {
   mockChrome()
 
-  mount(App)
-
-  await flushPromises()
+  render(<App />)
+  act(async () => {
+    await new Promise((resolve) => setTimeout(resolve, 0))
+  })
 
   expect(console.error).toBeCalledTimes(0)
   expect(console.warn).toBeCalledTimes(0)
