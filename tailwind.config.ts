a3784384cce035d255ec6a80c93ec0825f610dba
ngseke <ngseke@gmail.com>
2025-04-24 21:53:21 +0800

    chore: update tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,7 @@
 import type { Config } from 'tailwindcss'
 
 export default {
-  content: ['./src/**/*.{html,vue,js,ts}'],
+  content: ['./src/**/*.{html,vue,js,jsx,ts,tsx}'],
   theme: {
     container: {
       center: true,
