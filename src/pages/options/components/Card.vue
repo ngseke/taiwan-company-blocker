35a77e1bd47af1a31fac2faa748532d234df8d28
ngseke <ngseke@gmail.com>
2025-04-27 11:21:19 +0800

    refactor: remove vue components and composables

--- src/pages/options/components/Card.vue
+++
@@ -1,8 +0,0 @@
-<script setup lang="ts">
-</script>
-
-<template>
-  <div class="rounded-lg bg-neutral-900 p-6 shadow-2xl">
-    <slot />
-  </div>
-</template>
--- src/pages/options/components/Title.vue
+++
@@ -1,8 +0,0 @@
-<script setup lang="ts">
-</script>
-
-<template>
-  <h2 class="text-lg font-medium text-white/60">
-    <slot />
-  </h2>
-</template>
