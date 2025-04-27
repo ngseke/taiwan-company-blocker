a58efdae4ebe7b3a1ea2d8c416554db823b9000d
ngseke <ngseke@gmail.com>
2025-04-27 15:51:48 +0800

    feat: convert Card component to use forwardRef

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,10 +1,13 @@
-import { type ComponentProps } from 'react'
+import { forwardRef, type ComponentProps } from 'react'
 
-export function Card ({ className, ...restProps }: ComponentProps<'div'>) {
-  return (
-    <div
-      className="rounded-lg bg-neutral-900 p-6 shadow-2xl"
-      {...restProps}
-    />
-  )
-}
+export const Card = forwardRef<HTMLDivElement, ComponentProps<'div'>>(
+  function Card ({ className, ...restProps }, ref) {
+    return (
+      <div
+        ref={ref}
+        className="rounded-lg bg-neutral-900 p-6 shadow-2xl"
+        {...restProps}
+      />
+    )
+  }
+)
